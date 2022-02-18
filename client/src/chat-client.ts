beab5917dcdd796b385a89aa860c42b548cfca68
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-02-17 22:14:32 -0500

    chore(client): added doc comments

--- client/src/chat-client.ts
+++ client/src/chat-client.ts
@@ -3,13 +3,26 @@ import { SocketLineBuffer } from './socket-line-buffer'
 import { IOController } from './io-controller' 
 
 /**
- * 
+ * Represents chat client that connects to the TCP server.
  */
 export class ChatClient {
-   socket: net.Socket
-   lineBuffer: SocketLineBuffer
-   cli: IOController
+   /**
+    * Socket that connects to the chat server.
+    */
+   private socket: net.Socket
+   /**
+    * SocketLineBuffer instance to read incoming messages.
+    */
+   private lineBuffer: SocketLineBuffer
+   /**
+    * I/O controller to interact with the user.
+    */
+   private cli: IOController
 
+   /**
+    * @param host Hostname to connect to
+    * @param port Port number to connect to
+    */
    constructor(host: string, port: number) {
       this.cli = new IOController()
       this.cli.on('say', this.writeToServer.bind(this))
@@ -27,6 +40,14 @@ export class ChatClient {
       this.lineBuffer.on('line', this.register.bind(this))
    }
 
+   /**
+    * Called when the chat server sends its first message after
+    * establishing the connection.
+    * 
+    * Handles registration on the server.
+    * 
+    * @param message Incoming message from the server
+    */
    private async register(message: string) {
       console.log(message)
       const name = await this.cli.prompt()
@@ -38,25 +59,51 @@ export class ChatClient {
       await this.cli.runInputLoop()
    }
 
+   /**
+    * Called after successful registration on each incoming message from the server.
+    * 
+    * Displays incoming message to the user.
+    * 
+    * @param message Incoming message from the server
+    */
    private readLine(message: string) {
       this.cli.clearLine()
       this.cli.write(`${message}\n> `)
    }
 
+   /**
+    * Called when the client encountered an error.
+    * 
+    * @param error Encoutered error
+    */
    private handleError(error: Error) {
       this.cli.write(`Encountered error: ${error.message}\n`)
    }
 
+   /**
+    * Called when the user disconnected from the server and the connection
+    * socket was closed.
+    * 
+    * @param error Flag whether an error caused the 'close' event
+    */
    private handleClose(error: boolean) {
       if (error) return
       this.cli.clearLine()
       this.cli.write('Disconnected.\n')
    }
 
+   /**
+    * Sends a message to the chat server.
+    * 
+    * @param message Message to send
+    */
    private writeToServer(message: string) {
       this.socket.write(`${message}\n`, 'utf-8')
    }
 
+   /**
+    * Disconnect from the server.
+    */
    disconnect() {
       this.socket.destroy()
    }
--- client/src/io-controller.ts
+++ client/src/io-controller.ts
@@ -7,9 +7,19 @@ import { EventEmitter } from 'events'
  * By default uses stdin/stdout.
  */
 export class IOController extends EventEmitter {
+   /**
+    * Read stream used as input.
+    */
    private input: NodeJS.ReadStream
+   /**
+    * Write steam used as output.
+    */
    private output: NodeJS.WriteStream
 
+   /**
+    * @param input Read stream to use as input
+    * @param output Write stream to use as output
+    */
    constructor(input = stdin, output = stdout) {
       super()
       this.input = input
@@ -18,6 +28,12 @@ export class IOController extends EventEmitter {
       this.output.setEncoding('utf-8')
    }
 
+   /**
+    * Prompts the user asynchronously.
+    * 
+    * @param query Query to display before input
+    * @returns Input string provided by user
+    */
    prompt(query: string = '> '): Promise<string> {
       return new Promise(resolve => {
          const rl = readline.createInterface(this.input, this.output)
@@ -28,6 +44,14 @@ export class IOController extends EventEmitter {
       })
    }
 
+   /**
+    * Starts an async input loop that accepts input from the user.
+    * 
+    * - On '/q' emits 'close' event.
+    * - On '/h' shows a help message.
+    * - Any other input string emits 'say' event.
+    * - On SIGINT exits the loop, closes the stream, and propagates SIGINT to the process level.
+    */
    runInputLoop(): Promise<void> {
       return new Promise(resolve => {
          const rl = readline.createInterface(this.input, this.output)
@@ -64,10 +88,18 @@ export class IOController extends EventEmitter {
       })
    }
 
+   /**
+    * Writes the data to the underlying write stream.
+    * 
+    * @param data Data to write
+    */
    write(data: string) {
       this.output.write(data)
    }
 
+   /**
+    * Clears the line in the underlying write stream.
+    */
    clearLine() {
       this.output.clearLine(0)
       this.output.cursorTo(0)
